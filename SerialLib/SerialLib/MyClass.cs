e7fc2b8d07e97b8bc7079bfc08354e3243461ed1
reichon <renzero.symphony@gmail.com>
2017-07-11 02:43:28 +0900

    first commit

---
+++ SerialLib/SerialLib/MyClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+
+namespace SerialLib {
+	public class MyClass {
+		SerialPort serial;
+		byte[] buf;
+		string message;
+		Thread SerialThread;
+		bool isThreading = false;
+
+		public MyClass(string portName, int baudRate, int bufSize) {
+			buf = new byte[bufSize];
+			Init(portName, baudRate);
+			SerialThread = new Thread(this.UpdateData);
+		}
+
+		public void ThreadStart() {
+			SerialThread.Start ();
+		}
+
+		public void ThreadEnd() {
+			isThreading = false;
+			SerialThread.Abort ();
+			serial.Close ();
+		}
+
+		void Init(string portName, int baudRate) {
+			serial = new SerialPort (portName, baudRate, Parity.None, 8, StopBits.One);
+			try {
+				serial.Open ();
+				serial.DtrEnable = true;
+				serial.RtsEnable = true;
+				serial.DiscardInBuffer ();
+				serial.ReadTimeout = 50;
+			} catch (Exception e) {
+				serial = null;
+				return;
+			}
+		}
+
+		public string GetData() {
+			return message;
+		}
+
+		public void UpdateData() {
+			isThreading = true;
+
+			while (isThreading) {
+				//int read = 0;
+				try {
+					message = serial.ReadLine ();
+				} catch (TimeoutException e) {
+				}
+			}
+		}
+	}
+}
+
---
+++ Unity-Arduino/Assets/SerialTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class SerialTest : MonoBehaviour {
+	public GameObject rocket;
+	public static SerialLib.MyClass serial;
+
+	// Use this for initialization
+	void Start () {
+		serial = new SerialLib.MyClass ("/dev/tty.usbserial-AI04SV56", 115200, 256);
+		serial.ThreadStart ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string str = serial.GetData ();
+		if (str != null) {
+			int r = int.Parse (str);
+		}
+		Debug.Log (serial.GetData ());
+	}
+
+	void OnDestroy() {
+		serial.ThreadEnd ();
+	}
+}
